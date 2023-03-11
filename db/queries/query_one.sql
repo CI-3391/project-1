bd217994ba516a4f3ea2496fb93548d2ac3a23bb
Carlos Sivira <carlos.csivira@gmail.com>
2023-03-11 19:02:20 -0400

    query one

--- db/queries/query_one.sql
+++ db/queries/query_one.sql
@@ -1,3 +1,30 @@
+/* 
+    ¿Son siempre iguales los datos de la columna new_cases_smoothed con los
+    calculados en el script?
+
+
+    Salvo por 257 casos, los resultados calculados son iguales.
+
+    Se pueden encontra estos casos ejecutando
+
+    SELECT iso_code, date, new_cases, new_cases_smoothed, new_cases_smoothed_calc 
+    FROM svr.query_one
+    where new_cases_smoothed <> new_cases_smoothed_calc
+
+    Al validar los datos cercanos a cada fecha, usando por ejemplo 2021-10-13
+    que se corresponde con uno de los valores desiguales se observa que las 7
+    fechas previas no presentan valores diferentes y tampoco son null. Por lo
+    tanto se desconoce la razón de por qué estos 257 casos son diferentes.ABORT
+
+    Query utilizado para validar las fechas cercanas
+
+    SELECT iso_code, date, new_cases, new_cases_smoothed, new_cases_smoothed_calc 
+    FROM svr.query_one
+    where date < '2021-10-13'
+    order by date asc
+    limit 10
+ */
+
 create table svr.query_one (
 	iso_code varchar(100) references svr.country(iso_code) not null,
 	date date not null,
